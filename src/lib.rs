35ec8bb3ede4da437eab0ddecb98b69668e0441b
Ryan Weiler <ryanweiler92@gmail.com>
2025-09-18 00:01:54 -0400

    python bindings convert to dict

--- src/lib.rs
+++ src/lib.rs
@@ -4,38 +4,26 @@ pub mod models;
 pub use extract::YoutubeExtractor;
 pub use models::{VideoInfo, Comment, CommentContent};
 
-// Python bindings (optional feature `python`)
-// Build with: `maturin develop --features python` or `maturin build --features python`
 #[cfg(feature = "python")]
 mod python_bindings {
     use super::{YoutubeExtractor, VideoInfo, Comment};
     use pyo3::prelude::*;
+    use pyo3::types::PyDict;
 
     #[pyclass]
     #[derive(Clone)]
     struct PyVideoInfo {
-        #[pyo3(get)]
-        title: String,
-        #[pyo3(get)]
-        channel: String,
-        #[pyo3(get)]
-        channel_id: String,
-        #[pyo3(get)]
-        description: String,
-        #[pyo3(get)]
-        yt_id: String,
-        #[pyo3(get)]
-        views: u64,
-        #[pyo3(get)]
-        comment_count: u64,
-        #[pyo3(get)]
-        like_count: u64,
-        #[pyo3(get)]
-        video_thumbnail: String,
-        #[pyo3(get)]
-        upload_date: String,
-        #[pyo3(get)]
-        channel_thumbnail: String,
+        #[pyo3(get)] title: String,
+        #[pyo3(get)] channel: String,
+        #[pyo3(get)] channel_id: String,
+        #[pyo3(get)] description: String,
+        #[pyo3(get)] yt_id: String,
+        #[pyo3(get)] views: u64,
+        #[pyo3(get)] comment_count: u64,
+        #[pyo3(get)] like_count: u64,
+        #[pyo3(get)] video_thumbnail: String,
+        #[pyo3(get)] upload_date: String,
+        #[pyo3(get)] channel_thumbnail: String,
     }
 
     impl From<VideoInfo> for PyVideoInfo {
@@ -56,35 +44,44 @@ mod python_bindings {
         }
     }
 
+    #[pymethods]
+    impl PyVideoInfo {
+        fn to_dict<'py>(&self, py: Python<'py>) -> Bound<'py, PyDict> {
+            let d = PyDict::new_bound(py);
+            d.set_item("title", &self.title).unwrap();
+            d.set_item("channel", &self.channel).unwrap();
+            d.set_item("channel_id", &self.channel_id).unwrap();
+            d.set_item("description", &self.description).unwrap();
+            d.set_item("yt_id", &self.yt_id).unwrap();
+            d.set_item("views", self.views).unwrap();
+            d.set_item("comment_count", self.comment_count).unwrap();
+            d.set_item("like_count", self.like_count).unwrap();
+            d.set_item("video_thumbnail", &self.video_thumbnail).unwrap();
+            d.set_item("upload_date", &self.upload_date).unwrap();
+            d.set_item("channel_thumbnail", &self.channel_thumbnail).unwrap();
+            d
+        }
+        fn __repr__(&self) -> String {
+            format!("<PyVideoInfo title={!r} channel={!r} views={}>", self.title, self.channel, self.views)
+        }
+    }
+
     #[pyclass]
     #[derive(Clone)]
     struct PyComment {
-        #[pyo3(get)]
-        comment_id: String,
-        #[pyo3(get)]
-        channel_id: String,
-        #[pyo3(get)]
-        video_id: String,
-        #[pyo3(get)]
-        display_name: String,
-        #[pyo3(get)]
-        user_verified: bool,
-        #[pyo3(get)]
-        thumbnail: String,
-        #[pyo3(get)]
-        content: String,
-        #[pyo3(get)]
-        published_time: String,
-        #[pyo3(get)]
-        like_count: i32,
-        #[pyo3(get)]
-        reply_count: i32,
-        #[pyo3(get)]
-        comment_level: i32,
-        #[pyo3(get)]
-        reply_to: String,
-        #[pyo3(get)]
-        reply_order: i32,
+        #[pyo3(get)] comment_id: String,
+        #[pyo3(get)] channel_id: String,
+        #[pyo3(get)] video_id: String,
+        #[pyo3(get)] display_name: String,
+        #[pyo3(get)] user_verified: bool,
+        #[pyo3(get)] thumbnail: String,
+        #[pyo3(get)] content: String,
+        #[pyo3(get)] published_time: String,
+        #[pyo3(get)] like_count: i32,
+        #[pyo3(get)] reply_count: i32,
+        #[pyo3(get)] comment_level: i32,
+        #[pyo3(get)] reply_to: String,
+        #[pyo3(get)] reply_order: i32,
     }
 
     impl From<Comment> for PyComment {
@@ -107,6 +104,30 @@ mod python_bindings {
         }
     }
 
+    #[pymethods]
+    impl PyComment {
+        fn to_dict<'py>(&self, py: Python<'py>) -> Bound<'py, PyDict> {
+            let d = PyDict::new_bound(py);
+            d.set_item("comment_id", &self.comment_id).unwrap();
+            d.set_item("channel_id", &self.channel_id).unwrap();
+            d.set_item("video_id", &self.video_id).unwrap();
+            d.set_item("display_name", &self.display_name).unwrap();
+            d.set_item("user_verified", self.user_verified).unwrap();
+            d.set_item("thumbnail", &self.thumbnail).unwrap();
+            d.set_item("content", &self.content).unwrap();
+            d.set_item("published_time", &self.published_time).unwrap();
+            d.set_item("like_count", self.like_count).unwrap();
+            d.set_item("reply_count", self.reply_count).unwrap();
+            d.set_item("comment_level", self.comment_level).unwrap();
+            d.set_item("reply_to", &self.reply_to).unwrap();
+            d.set_item("reply_order", self.reply_order).unwrap();
+            d
+        }
+        fn __repr__(&self) -> String {
+            format!("<PyComment by={!r} likes={}>", self.display_name, self.like_count)
+        }
+    }
+
     fn run_async<F, T>(fut: F) -> Result<T, Box<dyn std::error::Error>>
     where
         F: std::future::Future<Output = Result<T, Box<dyn std::error::Error>>> + Send + 'static,
@@ -117,26 +138,28 @@ mod python_bindings {
         Ok(res)
     }
 
+    // Return dicts: (video_info: dict, comments: list[dict])
     #[pyfunction]
-    fn extract(video: &str, max_requests: Option<usize>) -> PyResult<(PyVideoInfo, Vec<PyComment>)> {
+    fn extract(py: Python<'_>, video: &str, max_requests: Option<usize>) -> PyResult<(PyObject, Vec<PyObject>)> {
         let extractor = YoutubeExtractor::new();
-        let video = video.to_owned(); // Convert to owned String
+        let video = video.to_owned();
         let fut = async move {
             let (info, mut comments) = extractor.extract(&video).await?;
-
-            // Limit comment fetch depth if requested by user by truncating for now
-            if let Some(_mr) = max_requests {
-                // The current Rust API already constrains requests internally when called directly; here we return what `extract` fetched.
-                // Future: wire `max_requests` through to `get_comments` if public API changes.
-            }
-
             let py_info: PyVideoInfo = info.into();
             let py_comments: Vec<PyComment> = comments.drain(..).map(Into::into).collect();
             Ok::<_, Box<dyn std::error::Error>>((py_info, py_comments))
         };
 
         match run_async(fut) {
-            Ok((info, comments)) => Ok((info, comments)),
+            Ok((info, comments)) => {
+                // convert to dicts tied to 'py' and move-owned as PyObject
+                let vi_dict = info.to_dict(py).into_py(py);
+                let comment_dicts = comments
+                    .into_iter()
+                    .map(|c| c.to_dict(py).into_py(py))
+                    .collect();
+                Ok((vi_dict, comment_dicts))
+            }
             Err(e) => Err(pyo3::exceptions::PyRuntimeError::new_err(format!("Extraction failed: {}", e))),
         }
     }
